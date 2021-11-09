985b2d38d7b089c4f1c84fe0286c6d2238f39187
jasonj <35540346@qq.com>
2021-11-09 18:31:24 +0800

    iterate threshold for best accuracy.

--- test_ocv_face_feture_on_lfw.py
+++ test_ocv_face_feture_on_lfw.py
@@ -3,6 +3,7 @@ import tqdm
 import argparse
 import cv2 as cv
 import random
+import numpy as np
 
 last_img1 = ''
 faces1 = None
@@ -98,7 +99,7 @@ def test_and_eval(lfw_list_file):
         if num_of_entries > 1:
             name_dir = entries_for_1_person[0]
 
-            for i in range(1, num_of_entries - 1):
+            for i in range(1, num_of_entries):
                 img1 = os.path.join(name_dir, entries_for_1_person[i])
 
                 # more than one image for this person
@@ -124,7 +125,7 @@ def test_and_eval(lfw_list_file):
                 num_compare_with_negative = 0
                 #TODO: Tune the steps
                 while num_compare_with_negative < 3:
-                    rand_idx = random.randint(0, num_of_person - 1)
+                    rand_idx = random.randint(0, num_of_person)
                     if rand_idx != idx :
                         entries_of_neg = all_data[rand_idx]
                         img2 = os.path.join(entries_of_neg[0], entries_of_neg[1])
@@ -195,10 +196,160 @@ def generate_list(imgs_dir, list_txt):
 
     f.close()
 
+def extract_feature_and_store(lfw_list_file):
+    #read lfw list
+    f = open(lfw_list_file, 'r')
+    state = 0
+    lines = f.readlines()
+    lines = list(map(lambda x: x.rstrip('\r\n'), lines))
+
+    all_data = []
+    for line in lines:
+        all_data.append(list(line.rstrip('\r\n').split(' ')))
+
+    # print('all data: {}'.format(all_data))
+
+    # LFW images are fixed image size
+    faceDetector = cv.FaceDetectorYN.create("/workspace/src/github/libfacedetection.train/tasks/task1/onnx/yunet.onnx", "", [250, 250])
+    faceRecognizer = cv.FaceRecognizerSF.create("./face_recognition_sface_2021sep.onnx", "")
+
+    num_of_person = len(all_data)
+    ibar = tqdm.tqdm(all_data)
+
+    all_features = []
+    for idx, entries_for_1_person in enumerate(ibar):
+        ibar.set_description('Extracting feature')
+
+        num_of_entries = len(entries_for_1_person)
+        if num_of_entries > 1:
+            name_dir = entries_for_1_person[0]
+
+            p_feature = []
+
+            for i in range(1, num_of_entries):
+                img = os.path.join(name_dir, entries_for_1_person[i])
+                img_data = cv.imread(img)
+
+                faceDetector.setInputSize((img_data.shape[1], img_data.shape[0]))
+                ret, faces = faceDetector.detect(img_data)
+
+                if faces is None:
+                    print('no faces found on face')
+                    continue
+
+                aligned_face = faceRecognizer.alignCrop(img_data, faces[0])
+
+                feature = faceRecognizer.feature(aligned_face)
+                p_feature.append(feature)
+
+            if len(p_feature) > 0 :
+                all_features.append(p_feature)
+
+    #
+    num_of_person = len(all_features)
+
+    ibar = tqdm.tqdm(all_features)
+
+    COS_SCORES = []
+    L2_SCORES = []
+    SAME_MASK = []
+
+    for idx, p_features in enumerate(ibar):
+        count_features = len(p_features)
+
+        ibar.set_description('Compare feature')
+
+        for i in range(0, count_features) :
+            feature1 = p_features[i]
+            #compare between same person
+            for j in range(i, count_features) :
+                feature2 = p_features[j]
+
+                cosine_score = faceRecognizer.match(feature1, feature2, 0)
+                l2_score = faceRecognizer.match(feature1, feature2, 1)
+
+                COS_SCORES.append(cosine_score)
+                L2_SCORES.append(l2_score)
+                SAME_MASK.append(1)
+
+            #compare between different persons
+            num_compare_with_negative = 0
+            #TODO: Tune the steps
+            while num_compare_with_negative < 3:
+                rand_idx = random.randint(0, num_of_person - 1)
+                if rand_idx != idx :
+                    feature2 = all_features[rand_idx][0]
+
+                    cosine_score = faceRecognizer.match(feature1, feature2, 0)
+                    l2_score = faceRecognizer.match(feature1, feature2, 1)
+
+                    COS_SCORES.append(cosine_score)
+                    L2_SCORES.append(l2_score)
+                    SAME_MASK.append(0)
+
+                    num_compare_with_negative = num_compare_with_negative + 1
+
+    cos_threshes = np.arange(0, 4, 0.01)
+    l2_threshes = np.arange(0, 4, 0.01)
+
+    ACCS = {}
+    ACCL = {}
+    TP = 0
+    TN = 0
+    for th in cos_threshes:
+        TP = 0
+        TN = 0
+        for idx in range(0, len(SAME_MASK)) :
+            if SAME_MASK[idx] == 1 :
+                if COS_SCORES[idx] >= th :
+                    TP = TP + 1
+            else:
+                if COS_SCORES[idx] < th :
+                    TP = TP + 1
+
+        ACCS['cos-' + str(th)] = '{:.2f}'.format(float(TP + TN) / len(SAME_MASK))
+
+    for th in l2_threshes:
+        TP = 0
+        TN = 0
+        for idx in range(0, len(SAME_MASK)) :
+            if SAME_MASK[idx] == 1 :
+                if L2_SCORES[idx] <= th :
+                    TP = TP + 1
+
+            else:
+                if L2_SCORES[idx] > th :
+                    TP = TP + 1
+
+        ACCL['l2-' + str(th)] = '{:.2f}'.format(float(TP + TN) / len(SAME_MASK))
+
+        max_accs = -1.0
+        max_accs_th = ''
+        for k, v in ACCS.items() :
+            print('Accuracy of cosine distance:')
+            print('{} : {}'.format(k, v))
+            if v > max_accs :
+                max_accs = v
+                max_accs_th = k
+
+        max_accl = -1.0
+        max_accl_th = ''
+        for k, v in ACCL.items() :
+            print('Accuracy of l2 distanec:')
+            print('{} : {}'.format(k, v))
+            if v > max_accl :
+                max_accl = v
+                max_accl_th = k
+
+        print('Max accuracy of cosine {}:{}'.format(max_accs_th, max_accs))
+        print('Max accuracy of l2 {}:{}'.format(max_accl_th, max_accl))
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--imgs', default='/workspace/dataset/lfw/lfw', help='path to images dir to be tested')
     parser.add_argument('-g', '--generate_list', action='store_true', help='generate lfw file list only')
+    parser.add_argument('-c', '--feature_compare', action='store_true', help='extract and compare feature')
 
     args = parser.parse_args()
 
@@ -207,4 +358,7 @@ if __name__ == '__main__':
     if args.generate_list :
         generate_list(args.imgs, list_txt)
     else:
-        test_and_eval(list_txt)
+        if args.feature_compare:
+            extract_feature_and_store(list_txt)
+        else:
+            test_and_eval(list_txt)
